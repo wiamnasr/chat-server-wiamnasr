835592a584a1fb621294007591148876f0e7ced6
Glitch (hello-express) <none>
2021-12-20 19:13:45 +0000

    levls 1,2,3

--- server.js
+++ server.js
@@ -1,29 +1,116 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!"
-}
+app.use(cors());
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage]
+const messages = [
+  {
+    id: 0,
+    from: "Bart",
+    text: "Welcome to CYF chat system!",
+  },
+];
 
+app.get("/messages/latest", (req, res) => {
+  const displayLatest = [];
+  const reversedMessagesArray = messages.reverse();
+  const limit =
+    reversedMessagesArray.length < 10 ? reversedMessagesArray.length : 10;
+  for (let i = 0; i < limit; i++) {
+    displayLatest.push(reversedMessagesArray[i]);
+  }
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
+  return res.send(displayLatest);
 });
 
+app.get("/messages/search", (req, res) => {
+  const searchTerm = req.query.text.toUpperCase();
 
+  if (searchTerm) {
+    const matchingTexts = messages.filter((message) =>
+      message.text.toUpperCase().includes(searchTerm)
+    );
+    if (matchingTexts.length > 0) {
+      return res.send(matchingTexts);
+    }
+    return res.status(404).json({
+      success: false,
+      msg: `No texts match your search term ("${searchTerm}")`,
+    });
+  }
+});
 
+app.delete("/messages/:id", (req, res) => {
+  const searchId = req.params.id;
+  if (searchId) {
+    const remainingMessages = messages.filter(
+      (message) => message.id !== Number(searchId)
+    );
+    if (remainingMessages.length > 0) {
+      return res.send(remainingMessages);
+    }
+    return res.status(404).json({
+      success: false,
+      msg: `Requested id does not exist (${searchId})`,
+    });
+  }
+});
 
+app.get("/messages/:id", (req, res) => {
+  const searchId = req.params.id;
+  if (searchId) {
+    const matchingId = messages.filter(
+      (message) => message.id === Number(searchId)
+    );
+    if (matchingId.length > 0) {
+      return res.send(matchingId);
+    }
+    return res.status(404).json({
+      success: false,
+      msg: `Requested id does not exist (${searchId})`,
+    });
+  }
+});
 
+app.post("/messages", (req, res) => {
+  if (!req.body.from || !req.body.text) {
+    return res
+      .status(404)
+      .json({ success: false, msg: "Name and Message cannot be empty" });
+  }
+  messages.push({
+    id: Math.random(),
+    from: req.body.from,
+    text: req.body.text,
+  });
 
-app.listen(process.env.PORT);
+  console.log(req.body);
+  return res.status(200).send(messages);
+});
+
+app.get("/messages", (req, res) => {
+  return res.status(200).json({ success: true, messages });
+});
+
+app.get("/", function (request, response) {
+  return response.status(200).sendFile(__dirname + "/index.html");
+});
+
+app.get("/*", function (request, response) {
+  return response
+    .status(404)
+    .json({ success: false, msg: "Not within my APIs reach..." });
+});
+
+// process.env.PORT
+
+app.listen(5000, () => console.log("listening at 5000..."));
